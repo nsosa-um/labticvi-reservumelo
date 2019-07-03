1b87a15b1a8429deba9b33fe0c70484bfa207e99
Bruno Camargo <brunocam1195@gmail.com>
2019-07-03 20:57:55 -0300

    Add Editoriales

---
+++ reservUMelo/dist/dtos/Editorial.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Editorial {
+    constructor($id, $nombre) {
+        this.id = $id;
+        this.nombre = $nombre;
+    }
+}
+exports.default = Editorial;
+//# sourceMappingURL=Editorial.js.map
